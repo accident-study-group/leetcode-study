c17f0eab3a26904cf4b47193c2269ea51358b081
홍종표 <54665433+jpoh281@users.noreply.github.com>
2022-08-05 00:57:57 +0900

    1차 수정

--- hdd/two_sum/two_sum.dart
+++ hdd/two_sum/two_sum.dart
@@ -1,17 +1,17 @@
+//contains의 시간복잡도가 1이 Set을 만들어
+//List에서 불필요한 반복을 돌 필요 없도록 변경
+
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-      
-    List<int> answer = [];
-      
-    for(int i = 0; i < nums.length; i++){
-        var result = nums.indexWhere((e) => e == target - nums[i], i+1);
-        if(result != -1){
-            answer.add(i);
-            answer.add(result);
-            break;
+    Set<int> numsSet = nums.toSet();
+    for (int i = 0; i < nums.length; i++) {
+      if (numsSet.contains(target - nums[i])) {
+        var result = nums.indexWhere((e) => e == target - nums[i], i + 1);
+        if (result != -1) {
+          return [i, result];
         }
+      }
     }
-      
-      return answer;
+    return [];
   }
 }
