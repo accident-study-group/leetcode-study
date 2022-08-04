737b0887e0ae7394432fecb87a2a704462411293
홍종표 <54665433+jpoh281@users.noreply.github.com>
2022-08-05 01:04:36 +0900

    1차 수정

--- hdd/two_sum/two_sum.kt
+++ hdd/two_sum/two_sum.kt
@@ -1,15 +1,21 @@
+// 다트와 마찬가지로
+// Set을 이용해 시간 절약
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        
+
         var answer = IntArray(2)
-        
+        var sets = nums.toSet()
+
         for(i : Int in 0 .. nums.size - 1){
-         val index = nums.indexOf(target - nums.get(i))
-         if(index != -1 && index != i){
-             answer.set(0,i)
-             answer.set(1,index)
-             break
-         }
+
+            if(sets.contains(target - nums.get(i)))   {
+                val index = nums.indexOf(target - nums.get(i))
+                if(index != -1 && index != i){
+                    answer.set(0,i)
+                    answer.set(1,index)
+                    break
+                }
+            }
         }
         return answer
     }
