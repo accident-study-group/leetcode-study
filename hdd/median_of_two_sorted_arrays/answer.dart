0ff2e55cc0bb2e51a5250e3fdb7a81ecce5baec8
jpoh281 <hjp4869@naver.com>
2022-08-13 23:46:25 +0900

    ✨수정

--- hdd/median_of_two_sorted_arrays/answer.dart
+++ hdd/median_of_two_sorted_arrays/answer.dart
@@ -1,38 +1,39 @@
 class Solution {
   double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
-    int m = nums1.length;
-    int n = nums2.length;
+
+    var totalLength = nums1.length + nums2.length;
 
     List<int> sortedList = [];
 
-    while(sortedList.length < m+n){
-
-      if(nums1.isEmpty){
-        sortedList.addAll(nums2);
-      }else if(nums2.isEmpty){
-        sortedList.addAll(nums1);
-      } else {
-
+    while(sortedList.length < totalLength){
+      if(nums1.isNotEmpty && nums2.isNotEmpty)
         switch(nums1.first.compareTo(nums2.first)){
           case 1:
             sortedList.insert(sortedList.length, nums2.removeAt(0));
             break;
           default:
             sortedList.insert(sortedList.length, nums1.removeAt(0));
+            break;
         }
+
+      if(nums1.isEmpty){
+        sortedList.addAll(nums2);
+      }else if(nums2.isEmpty){
+        sortedList.addAll(nums1);
       }
     }
 
 
-    var length = sortedList.length;
 
-    if(length.isOdd){
-      return sortedList[(length ~/ 2)].toDouble();
+    if(totalLength.isOdd){
+      return sortedList[(totalLength ~/ 2)].toDouble();
     }else {
-      return (sortedList[length ~/ 2 - 1] + sortedList[length ~/ 2]) / 2;
+      return (sortedList[totalLength ~/ 2 - 1] + sortedList[totalLength ~/ 2]) / 2;
     }
   }
 }
+
+
 // 1차 시도
 // Runtime: 927 ms, faster than 25.00% of Dart online submissions for Median of Two Sorted Arrays.
 // Memory Usage: 148.4 MB, less than 100.00% of Dart online submissions for Median of Two Sorted Arrays.
