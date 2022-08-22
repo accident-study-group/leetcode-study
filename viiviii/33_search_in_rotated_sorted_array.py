ee7a3cae997ec93e317dfc1f7af2719ce7b496c9
viiviii <viiiix.viiviii@gmail.com>
2022-08-22 14:10:50 +0900

    ♻️ 33. 리팩토링
    - 81처럼 메서드 분리해서 같은 패턴으로 변경

--- viiviii/33_search_in_rotated_sorted_array.py
+++ viiviii/33_search_in_rotated_sorted_array.py
@@ -1,11 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         start, end = 0, len(nums) - 1
-        while start < end:
-            i = start + int((end - start) // 2)
-            if nums[i] == target:
-                return i
-            l, m, r = nums[start], nums[i], nums[end]
-            search_left = l <= target < m if l <= m else l <= target or target < m
-            start, end = (start, i) if search_left else (i + 1, end)
-        return start if nums[start] == target else -1
+        while start <= end:
+            mid = start + ((end - start) // 2)
+            if nums[mid] == target:
+                return mid
+            l, m, r = nums[start], nums[mid], nums[end]
+            if self.in_left(l, m, target):
+                start, end = start, mid - 1
+            elif self.in_right(r, m, target):
+                start, end = mid + 1, end
+            else:
+                return -1
+
+    def in_left(self, l: int, m: int, x: int) -> bool:
+        if l > m:
+            return l <= x or x < m
+        return l <= x and x < m
+
+    def in_right(self, r: int, m: int, x: int) -> bool:
+        if r < m:
+            return m < x or x <= r
+        return m < x and x <= r
