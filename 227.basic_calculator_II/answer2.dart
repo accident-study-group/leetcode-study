17418c0ae4ea991f923f9ec73f6eadacdd7d06b9
jpoh281 <hjp4869@naver.com>
2022-08-28 23:40:08 +0900

    변수명 수정 lastPrimeIndex ->  numberStartingIndex

--- 227.basic_calculator_II/answer2.dart
+++ 227.basic_calculator_II/answer2.dart
@@ -8,13 +8,13 @@ class Solution {
 
     // 마지막 기호의 Index
     // 해당 위치부터 다음 기호 혹은 String 의 길이까지 숫자를 substring 한다.
-    int lastPrimeIndex = -1;
+    int numberStartingIndex = 0;
 
 
     for(int i = 0; i < s.length; i++){
       // 현재 인덱스의 값이 기호인 경우
       if(primes.contains(s[i])){
-        var number = int.parse(s.substring(lastPrimeIndex+1,i).trim());
+        var number = int.parse(s.substring(numberStartingIndex,i).trim());
 
         // 스택이 비어있지 않다면
         if(stack.isNotEmpty){
@@ -33,11 +33,11 @@ class Solution {
 
         stack.add(number);
         stack.add(s[i]);
-        lastPrimeIndex = i;
+        numberStartingIndex = i+1;
       }
 
       if(i == s.length-1){
-        var number = int.parse(s.substring(lastPrimeIndex+1).trim());
+        var number = int.parse(s.substring(numberStartingIndex).trim());
         if(stack.isNotEmpty && priorityPrimes.contains(stack.last)){
           var lastPrime = stack.removeLast();
           var lastNumber = stack.removeLast();
