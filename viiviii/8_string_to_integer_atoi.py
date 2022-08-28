3e54f98a2c8792e35660afb0339e11f1605985a2
viiviii <viiiix.viiviii@gmail.com>
2022-08-28 22:46:42 +0900

    ♻️ 8. 리팩토링
    - python은 +,-,공백을 포함한 문자열도 int로 변경해주므로 try-catch와 함께 사용하였음

--- viiviii/8_string_to_integer_atoi.py
+++ viiviii/8_string_to_integer_atoi.py
@@ -1,32 +1,22 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        start = end = 0
         a = s.lstrip()
         if not a:
             return 0
-        if a[0] in ('-', '+'):
-            start = end = 1
-        for c in a[start:]:
+        end = 1
+        for c in a[i:]:
             if not c.isdigit():
-                return self.a_to_i_and_clamp(a[start:end], sign=a[:start])
+                break
             end += 1
-        return self.a_to_i_and_clamp(a[start:end], sign=a[:start])
+        return self.clamp(self.a_to_i(a[:end]))
 
-    def a_to_i_and_clamp(self, a: str, sign: str) -> int:
-        i = self.a_to_i(a, sign)
-        return self.clamp(i)
-
-    def a_to_i(self, a: str, sign: str) -> int:
-        i = int(a) if a else 0
-        is_positive = not sign or sign == '+'
-        return i if is_positive else i * -1
+    def a_to_i(self, a: str) -> int:
+        try:
+            return int(a)
+        except ValueError:
+            return 0
 
     def clamp(self, i: int) -> int:
         MIN = -1 * (2 ** 31)
         MAX = (2 ** 31) - 1
-        if i < MIN:
-            return MIN
-        elif i > MAX:
-            return MAX
-        else:
-            return i
+        return min(max(i, MIN), MAX)
