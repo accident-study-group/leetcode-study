fd172fe81f781c8f6fad287b51c44161bdfb0a6b
jpoh281 <hjp4869@naver.com>
2022-09-03 01:39:30 +0900

    불필요한 덧셈 작업 삭제, if -> switch로 변경

--- hdd/54_Spiral_Matrix /answer.dart
+++ hdd/54_Spiral_Matrix /answer.dart
@@ -8,7 +8,6 @@
 // 세로 <= 가로인 경우 방향 세로 * 2 - 1
 // 세로 > 가로인 경우 (세로 * 2 - 1) - ((세로 - 가로) * 2 - 1)
 
-
 class Solution {
   List<int> spiralOrder(List<List<int>> matrix) {
     List<int> snailArray = [];
@@ -18,47 +17,43 @@ class Solution {
     int padding = 0;
 
     var count = 0;
-    if(height - width > 0){
+    if (height > width) {
       count = (height * 2 - 1) - ((height - width) * 2 - 1);
-    }else {
+    } else {
       count = height * 2 - 1;
     }
 
-
-    for(int i = 0; i < count; i++){
-      if(i % 4 == 0){
-        for(int x = padding ~/ 2; x < width - padding ~/ 2; x++){
-          snailArray.add(matrix[0 + padding ~/ 2][x]);
-        }
-
-        padding++;
-      }
-
-      else if(i % 4 == 1){
-        for(int y = padding ~/ 2 + 1; y < height - padding ~/ 2; y++){
-          snailArray.add(matrix[y][width - (padding ~/ 2 + 1)]);
-        }
-      }
-
-      else if(i % 4 == 2){
-        for(int x = width - (padding ~/ 2 + 2); x >= 0 + padding ~/ 2; x--){
-          snailArray.add(matrix[height - (padding ~/ 2 + 1)][x]);
-        }
-
-        padding ++;
-      }
-
-      else if(i % 4 == 3){
-        for(int y = height - (padding ~/ 2 + 1); y >= 0 + padding ~/ 2; y--){
-          snailArray.add(matrix[y][0 + (padding ~/ 2 -1)]);
-        }
+    for (int i = 0; i < count; i++) {
+      switch (i % 4) {
+        case 0:
+          for (int x = padding ~/ 2; x < width - padding ~/ 2; x++) {
+            snailArray.add(matrix[padding ~/ 2][x]);
+          }
+          padding++;
+          break;
+        case 1:
+          for (int y = padding ~/ 2 + 1; y < height - padding ~/ 2; y++) {
+            snailArray.add(matrix[y][width - (padding ~/ 2 + 1)]);
+          }
+          break;
+        case 2:
+          for (int x = width - (padding ~/ 2 + 2); x >= padding ~/ 2; x--) {
+            snailArray.add(matrix[height - (padding ~/ 2 + 1)][x]);
+          }
+          padding++;
+          break;
+        default:
+          for (int y = height - (padding ~/ 2 + 1); y >= padding ~/ 2; y--) {
+            snailArray.add(matrix[y][padding ~/ 2 - 1]);
+          }
+          break;
       }
     }
-
     return snailArray;
-
   }
 }
+
+
 
 // 최고 기록
 // Runtime: 282 ms, faster than 100.00% of Dart online submissions for Spiral Matrix.
