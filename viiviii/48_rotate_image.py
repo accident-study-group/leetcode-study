7d6b2024d6f3098f19c4418bc15e020336c88411
viiviii <viiiix.viiviii@gmail.com>
2022-09-05 19:21:13 +0900

    ♻️ 48번 회전 각도별로 돌릴 수 있게 변경

--- viiviii/48_rotate_image.py
+++ viiviii/48_rotate_image.py
@@ -1,17 +1,26 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        self.rotate_with_degrees(matrix, 90)
+    
+    def rotate_with_degrees(self, matrix, degrees):
+        assert not degrees % 90
+        degrees = degrees if not 360 % degrees else -1 * (360 % degrees)
+        for _ in range(abs(degrees // 90)):
+            if degrees > 0:         
+                self.transpose(matrix)
+                self.reverse(matrix)
+            else:
+                self.reverse(matrix)           
+                self.transpose(matrix)
+  
+    def transpose(self, matrix):
         n = len(matrix)
-        depth = n // 2
-        while depth > 0:
-            depth -= 1
-            next_ = depth
-            while next_ < n - depth - 1:
-                i, j = depth, next_
-                current = matrix[i][j]
-                for _ in range(4):
-                    i, j = j, n-1-i
-                    tmp = matrix[i][j]
-                    matrix[i][j] = current
-                    current = tmp
-                next_ += 1
-        return matrix
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+                
+    def reverse(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
