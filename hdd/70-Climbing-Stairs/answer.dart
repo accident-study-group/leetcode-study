cb81fb9c16eaed585084388beba1e791f213bfea
jpoh281 <hjp4869@naver.com>
2022-09-06 02:14:11 +0900

    중복 코드 제거

--- hdd/70-Climbing-Stairs/answer.dart
+++ hdd/70-Climbing-Stairs/answer.dart
@@ -8,23 +8,17 @@ class Solution {
     int twoCounts = 0;
     int sum = 0;
 
-    while(twoCounts <= (n / 2)){
-      sum += combination(n - twoCounts, twoCounts);
+    while (twoCounts <= (n / 2)) {
+      sum += combination(n - twoCounts, twoCounts).round();
       twoCounts++;
     }
     return sum;
   }
 
-  // 콤비네이션을 간단하게 구현하면 문제 끝날것같다.
-  int combination(int n, int r){
-    if(r == 0 || n == r) return 1;
-    if(r == 1) return n;
-    return factorial(n,r).round();
-  }
-
-
-  num factorial(int n, int r){
-    if(r == 1) return n;
-    return n / r * factorial(n-1, r-1);
+  // 계산 결과는 조합(combination)과 같음.
+  num combination(int n, int r) {
+    if (r == 0 || n == r) return 1;
+    if (r == 1 || n - r == 1) return n;
+    return n / r * combination(n - 1, r - 1);
   }
 }
