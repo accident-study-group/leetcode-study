7743f02ae362aa9fec3f6eceda0bcc3455357d2b
jpoh281 <hjp4869@naver.com>
2022-09-17 13:23:11 +0900

    중단

---
+++ hdd/98_Validate_Binary_Search_Tree/answer.dart
@@ -0,0 +1,65 @@
+class Solution {
+  bool isValidBST(TreeNode? root) {
+    return validCheck(root, null);
+  }
+
+  validCheck(TreeNode? node, TreeNode? key) {
+    if (node == null) {
+      return true;
+    }
+
+    var leftValid = true;
+    var rightValid = true;
+
+    if(key == null) {
+      if (node.left != null) {
+        if (node.val <= (node.left!.val)) {
+          return false;
+        }
+        leftValid = validCheck(node.left, node);
+      }
+
+      if (node.right != null) {
+        if (node.val >= (node.right!.val)) {
+          return false;
+        }
+        rightValid = validCheck(node.right, node);
+      }
+    } else {
+      if(node.val < key.val){
+        if (node.left != null) {
+          if (node.val < (node.left!.val) || key.val < (node.left!.val)) {
+            return false;
+          }
+          leftValid = validCheck(node.left, key);
+        }
+
+        if (node.right != null) {
+          if (node.val > (node.right!.val) || key.val < (node.right!.val)) {
+            return false;
+          }
+          rightValid = validCheck(node.right, key);
+        }
+      } else if(node.val > key.val){
+        if (node.left != null) {
+          if (node.val < (node.left!.val) || key.val > (node.left!.val)) {
+            return false;
+          }
+          leftValid = validCheck(node.left, key);
+        }
+
+        if (node.right != null) {
+          if (node.val > (node.right!.val) || key.val > (node.right!.val)) {
+            return false;
+          }
+          rightValid = validCheck(node.right, key);
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return leftValid && rightValid;
+  }
+}
+
