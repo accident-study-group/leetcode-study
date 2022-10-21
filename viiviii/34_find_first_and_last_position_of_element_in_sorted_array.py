f46b391dfce563d7bce34e1b6b141ed1ca7596a5
viiviii <viiiix.viiviii@gmail.com>
2022-10-21 13:54:34 +0900

    ♻️ 34. Find First and Last Position of Element in Sorted Array
    - arr.index 제거하고 for문 2번 돌도록 변경

--- viiviii/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ viiviii/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,17 +1,35 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = last = -1
-        try:
-            first = last = nums.index(target)
-        except ValueError:
-            return [first, last]
-        
-        start, end = first, len(nums) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
-                last = mid
-                start = mid + 1
-            else:
-                end = mid -1
+        first = search_to_left(nums, target)
+        last = search_to_right(nums, target)
         return [first, last]
+
+
+def search_to_left(nums, target):
+    first = -1
+    start, end = 0, len(nums) - 1
+    while start <= end:
+        mid = start + ((end - start) // 2)
+        if nums[mid] == target:
+            first = mid
+            end = mid - 1
+        elif nums[mid] < target:
+            start = mid + 1
+        elif nums[mid] > target:
+            end = mid - 1
+    return first
+
+
+def search_to_right(nums, target):
+    last = -1
+    start, end = 0, len(nums) - 1
+    while start <= end:
+        mid = start + ((end - start) // 2)
+        if nums[mid] == target:
+            last = mid
+            start = mid + 1
+        elif nums[mid] < target:
+            start = mid + 1
+        elif nums[mid] > target:
+            end = mid - 1
+    return last
